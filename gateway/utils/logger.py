0ef4f8ed10375eac0d56f53bbe556eb31f350d98
thjung123 <jeothen@gmail.com>
2025-10-19 16:19:14 +0900

    [JIRA-7] refactor: use ContextVar for request_id and propagate logs (#12)

--- gateway/utils/logger.py
+++ gateway/utils/logger.py
@@ -1,43 +1,33 @@
 import logging
 import sys
-from logging.handlers import QueueHandler, QueueListener
-from multiprocessing import Queue
-from typing import Optional
-
-_log_queue: Optional[Queue] = None
-_listener: Optional[QueueListener] = None
+from pythonjsonlogger import jsonlogger
+from gateway.middlewares.request_id import request_id_ctx
 
 
-class SafeFormatter(logging.Formatter):
-    def format(self, record):
-        if not hasattr(record, "request_id"):
-            record.request_id = "-"
-        return super().format(record)
+class SafeJsonFormatter(jsonlogger.JsonFormatter):
+    def process_log_record(self, log_record):
+        log_record["request_id"] = request_id_ctx.get() or "-"
+        return log_record
 
 
-def setup_async_logger(
-    name: str = "gateway", level: int = logging.INFO
-) -> logging.Logger:
-    global _log_queue, _listener
+def setup_logger(level=logging.INFO) -> logging.Logger:
+    logger = logging.getLogger()
+    gunicorn_logger = logging.getLogger("gunicorn.access")
 
-    if _log_queue is None:
-        _log_queue = Queue(-1)
-
-        stream_handler = logging.StreamHandler(sys.stdout)
-        stream_handler.setFormatter(
-            SafeFormatter("[%(asctime)s] [%(levelname)s] [%(request_id)s] %(message)s")
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(
+            SafeJsonFormatter(
+                "%(asctime)s %(levelname)s %(name)s %(process)d %(request_id)s %(message)s"
+            )
         )
-        _listener = QueueListener(
-            _log_queue, stream_handler, respect_handler_level=True
-        )
-        _listener.start()
+        logger.addHandler(handler)
+        logger.setLevel(level)
 
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-    logger.addHandler(QueueHandler(_log_queue))
-    logger.propagate = False
+        logger.parent = gunicorn_logger
+        logger.propagate = True
 
     return logger
 
 
-logger = setup_async_logger()
+logger = setup_logger(level=logging.INFO)
